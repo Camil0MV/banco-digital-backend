03d6156a8d8afe303ea57628f2641c9c32005490
Camilo Mosquera <camilomosquerav@gmail.com>
2026-04-29 22:05:10 -0500

    fix(test): agregar perfil test con H2 y corregir NPE en CuentaServiceTest

--- src/test/java/co/edu/udea/bancodigital/BancodigitalApplicationTests.java
+++ src/test/java/co/edu/udea/bancodigital/BancodigitalApplicationTests.java
@@ -2,8 +2,10 @@ package co.edu.udea.bancodigital;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class BancodigitalApplicationTests {
 
 	@Test
